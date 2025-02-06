873adf6ce00c82fefe66085d360c90c94d2ec280
ChrisSenina <ChrisSenina@users.noreply.github.com>
2025-02-06 23:09:07 +0000

    Deploying to production from @ ChrisSenina/htdevTEST@036ebf87438539c38f386091407d0c0f487a8e6e 🚀

---
+++ source/source/js/main.js
@@ -0,0 +1,51 @@
+import {mobileVhFix} from './utils/mobile-vh-fix.js';
+import {findVideos} from './modules/video.js';
+import {playAccordion} from './modules/accordion.js';
+import {initSliderAdv, initSliderPromo} from './modules/slider.js';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  mobileVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    findVideos();
+    playAccordion();
+    initSliderAdv();
+    initSliderPromo();
+  });
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используейтся matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
